126bf66df11984fd989644f81fc83f804797edb9
liottar <robert.liotta@cevalogistics.com>
2020-06-15 07:29:37 -0400

    correction
    Former-commit-id: 235f7b5781a785d1dacb0b922aa63edf38c4cdd4

--- charts.sh
+++ charts.sh
@@ -10,11 +10,11 @@ sed -ie "s/^version.*$/version: $CV/g" chart/Chart.yaml
 for chart in chart
 do
   echo Processing: $chart
-  helm3 lint $chart
-  helm3 package $chart
+  helm lint $chart
+  helm package $chart
 done
 
-helm3 repo index --url https://BobDotMe.github.io/bob . 2>/dev/null
+helm repo index --url https://BobDotMe.github.io/bob . 2>/dev/null
 
 mv index.yaml ../docs/.
 mv *.tgz ../docs/.

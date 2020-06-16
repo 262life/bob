191b8d5000bf8d532d4ead8cd4d1ea52cde1c81f
liottar <robert.liotta@cevalogistics.com>
2020-06-16 15:53:43 -0400

    fixes

--- plugins.sh
+++ plugins.sh
@@ -4,7 +4,7 @@
 CV=$(cat VERSION)
 echo Version: $CV
 
-if grep "darwin" <<< "$OSTYPE"; then sedopts='-i .bak'; else sedopts='-i'; fi
+if echo "$OSTYPE" | grep -q 'darwin' ; then sedopts='-i .bak'; else sedopts='-i'; fi
 
 cd plugins/kubectl
 

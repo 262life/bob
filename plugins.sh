44c9f294937cbba201bde7ee7b45065a39554936
liottar <robert.liotta@cevalogistics.com>
2020-06-16 15:45:19 -0400

    platform fix
    Former-commit-id: ab80f41f4afe91c64ce292b3a10fc9eb4cede351

--- plugins.sh
+++ plugins.sh
@@ -1,11 +1,12 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 
 CV=$(cat VERSION)
 echo Version: $CV
-pwd
+
+if grep "darwin" <<< "$OSTYPE"; then sedopts='-i .bak'; else sedopts='-i'; fi
 
 cd plugins/kubectl
 
-sed -i .bak  -e "s/^echo.*$/echo 'version: $CV'/g" kubectl-bob-version
+sed ${sedopts} -e "s/^echo.*$/echo 'version: $CV'/g" kubectl-bob-version
 

b557e2b33abddb5ad54c97a48bd49fe009efdb44
liottar <robert.liotta@cevalogistics.com>
2020-06-18 17:20:19 -0400

    README FIX
    Former-commit-id: 0e0923bb2b01c2689bcd609f3178978e85eafe3b

--- .github/workflows/update_readme.sh
+++ .github/workflows/update_readme.sh
@@ -7,5 +7,5 @@ echo Version: $CV
 if echo "$OSTYPE" | grep -q 'darwin' ; then sedopts='-i .bak'; else sedopts='-i'; fi
 
 
-sed ${sedopts} -e "1,2 s/v.*$/$CV)/g" README.md
+sed ${sedopts} -e "1 s/v.*$/$CV)/g" README.md
 

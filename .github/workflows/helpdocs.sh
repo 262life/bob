fe81af193a59c3648eba06ace0a0b37ae6b885f0
liottar <robert.liotta@cevalogistics.com>
2020-06-24 11:33:52 -0400

    fixes
    Former-commit-id: 5424dbae3b9e1c4277903107ec7898d2f8e2bfde

--- .github/workflows/helpdocs.sh
+++ .github/workflows/helpdocs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "# BOB plugins" > docs/commands.md
-echo "" > docs/commands.md
+echo ""              > docs/commands.md
 
 P="plugins/kubectl"
 for doc in $P/kubectl-bob*
@@ -9,5 +9,5 @@ do
   mdname=$(basename $doc | sed -e "s/kubectl-bob-//g")
   if [ "$mdname" = "kubectl-bob" ];then mdname=bob; fi
   $doc --help 2>docs/commands/$mdname.md
-  echo "[$mdname](commands/$mdname.md)" >> docs/commands/md
+  echo "[$mdname](commands/$mdname.md)" >> docs/commands.md
 done

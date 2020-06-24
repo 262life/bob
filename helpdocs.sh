8ead81027108d899f9b5d5bb93fdd976c038266a
liottar <robert.liotta@cevalogistics.com>
2020-06-24 10:27:43 -0400

    Docs

--- helpdocs.sh
+++ helpdocs.sh
@@ -4,6 +4,7 @@ P="plugins/kubectl"
 for doc in $P/kubectl-bob*
 do
 mdname=$(basename $doc | sed -e "s/kubectl-bob-//g")
+if [ "$mdname" = "kubectl-bob" ];then mdname=bob; fi
 $doc --help 2>docs/commands/$mdname.md
 done
 

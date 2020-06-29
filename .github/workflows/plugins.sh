bc3869a6904c5b392c0d04afb26252c43080b6cf
liottar <robert.liotta@cevalogistics.com>
2020-06-28 21:17:00 -0400

    Enhancements
    Former-commit-id: 60942d6cb220c14f296dd9d52ee070fc7d2f612d

--- .github/workflows/plugins.sh
+++ .github/workflows/plugins.sh
@@ -6,14 +6,14 @@ echo Version: $CV
 
 if echo "$OSTYPE" | grep -q 'darwin' ; then sedopts='-i .bak'; else sedopts='-i'; fi
 
-cd plugins/kubectl
+cd plugins/kubectl || exit
 
 sed ${sedopts} -e "s/^echo.*$/echo 'version: $CV'/g" kubectl-bob-version
 sed ${sedopts} -e "s/bob:.*$/bob:$CV/g" kubectl-bob
 sed ${sedopts} -e "s/bob-proxy:.*/bob-proxy:$CV/g" kubectl-bob
 
 echo result:
-echo kubectl-bob-version: $(cat kubectl-bob-version | grep 'version:')
-echo kubectl-bob bob    : $(cat kubectl-bob | grep 'bob:')
-echo kubectl-bob proxy  : $(cat kubectl-bob | grep 'bob-proxy:')
+echo "kubectl-bob-version: $(grep 'version:' kubectl-bob-version)"
+echo "kubectl-bob bob    : $(grep 'bob:' kubectl-bob)"
+echo "kubectl-bob proxy  : $(grep 'bob-proxy:' kubectl-bob)"
 

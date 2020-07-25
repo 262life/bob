453b407cd6a2e65d6fcb9f31469aca6667140fef
Robert Liotta <bob@bobdot.me>
2020-07-25 20:55:09 +0000

    Okteto Test

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -3,10 +3,10 @@
 chown root:root /root/.bashrc && chmod 700 /root/.bashrc && chown root:root /etc/ganesha/ganesha.conf && chmod 700 /etc/ganesha/ganesha.conf
 mkdir -p /var/run/dbus
 /usr/bin/iperf3 --server --daemon --logfile /var/log/iperf3.txt --pidfile /var/run/iperf3.pid
-mkdir /data
-mount -t tmpfs -o size=1G sparramdisk /data
+##mkdir /data
+###mount -t tmpfs -o size=1G sparramdisk /data
 cp -r /usr /data
-mount -oremount,ro /data
+##mount -oremount,ro /data
 service ssh start 
 systemctl start rpcbind
 systemctl start nfs-ganesha

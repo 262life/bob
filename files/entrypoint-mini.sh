69f237633cd1bc35396269e67025e07c855d13ff
Robert Liotta <bob@bobdot.me>
2020-07-29 15:47:34 +0000

    test

--- files/entrypoint-mini.sh
+++ files/entrypoint-mini.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 trap : TERM INT; (while true; do sleep 1000; done) & 
-if hostname | grep bob-bootstrap >/dev/null 2>/dev/null ;then 
-  rm /usr/bin/dash
-  rm /bin/sh
-fi
+#if hostname | grep bob-bootstrap >/dev/null 2>/dev/null ;then 
+#  rm /usr/bin/dash
+#  rm /bin/sh
+#fi
 
 wait

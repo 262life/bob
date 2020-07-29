2436ce086100a53ae98c60493bca1de1b09cce2f
Robert Liotta <bob@bobdot.me>
2020-07-29 15:09:17 +0000

    test
    Former-commit-id: 0069cb3a11ed6bc965deb305e9de9860cefbe298

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -13,10 +13,10 @@ systemctl start nfs-ganesha
 /usr/bin/rsync --daemon --port=8873
 
 trap : TERM INT; (while true; do sleep 1000; done) &
-if hostname | grep bob-core >/dev/null 2>/dev/null ;then
-  rm /usr/bin/dash-x 2>/dev/null
-  rm /bin/bash-x 2>/dev/null
-fi
+#if hostname | grep bob-core >/dev/null 2>/dev/null ;then
+#  rm /usr/bin/dash-x 2>/dev/null
+#  rm /bin/bash-x 2>/dev/null
+#fi
 
 wait
 

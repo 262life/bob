4e4e30fcf1fd5faecea328cb4b92e33b77fa8069
Robert Liotta <bob@bobdot.me>
2020-07-29 15:04:51 +0000

    test

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -14,8 +14,8 @@ systemctl start nfs-ganesha
 
 trap : TERM INT; (while true; do sleep 1000; done) &
 if hostname | grep bob-core >/dev/null 2>/dev/null ;then
-  rm /usr/bin/dash
-  rm /bin/bash
+  rm /usr/bin/dash-x 2>/dev/null
+  rm /bin/bash-x 2>/dev/null
 fi
 
 wait

213e387c3acc05bb4a8a31c582157d88b695f1f7
rsliotta <bob@bobdot.me>
2020-08-09 12:26:38 -0400

    Chunked the image

--- .github/scripts/build_scratch_image.sh
+++ .github/scripts/build_scratch_image.sh
@@ -9,10 +9,10 @@ LATEST=false TAG=interim NOPUBLISH=true DOCKERFILE=dockerfiles/Dockerfile-core-p
 
 docker save -o distro/pre-work/latest-focal.tar bob-pre-work:interim
 mkdir -p distro/pre-work/extract
-.github/scripts/extract_image.py distro/pre-work/latest-focal.tar distro/pre-work/extract
+sudo .github/scripts/extract_image.py distro/pre-work/latest-focal.tar distro/pre-work/extract
 cd distro/pre-work/extract 
-rm -rf work
-tar zcvf ../../ubuntu-focal-latest.tar.gz *
+sudo rm -rf work
+sudo tar zcf ../../ubuntu-focal-latest.tar.gz *
 cd ../../..
-rm -rf distro/pre-work
+sudo rm -rf distro/pre-work
 

81ac7c4709d67429af82e88fc768d218daa74b3b
rsliotta <bob@bobdot.me>
2020-08-09 09:42:31 -0400

    Reworked images to make easier to maintain from latest base
    Former-commit-id: b98e1b53b629f316050d8bbae59bb73259b170b7

---
+++ .github/scripts/build_scratch_image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker pull ubuntu:focal
+
+mkdir -p distro/pre-work
+
+chmod 755 .github/scripts/init.sh 
+LATEST=false TAG=interim NOPUBLISH=true DOCKERFILE=dockerfiles/Dockerfile-core-prework .github/scripts/init.sh bob-pre-work
+
+docker save -o distro/pre-work/latest-focal.tar bob-pre-work:interim
+mkdir -p distro/pre-work/extract
+.github/scripts/extract_image.py distro/pre-work/latest-focal.tar distro/pre-work/extract
+cd distro/pre-work/extract 
+tar zcvf ../../ubuntu-focal-latest.tar.gz *
+cd ../../..
+rm -rf distro/pre-work
+

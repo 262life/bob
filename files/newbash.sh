93f3c976ee5b6ad4a70ad8d7acd4c166be635e5b
Robert Liotta <bob@bobdot.me>
2020-08-10 22:46:59 +0000

    changes

--- files/newbash.sh
+++ files/newbash.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo "Bootstrapping BOB ..." >&2
-if /usr/bin/rsync -a "rsync://bob.${BOB_SERVICE_NAMESPACE}.svc:8873/core/usr/" "/usr/" ; then exec /bin/bash "$@"; fi
+echo "Bootstrapping BoB ..." >&2
+if /usr/bin/rsync -a "rsync://bob.${BOB_SERVICE_NAMESPACE}.svc:8873/core/usr/" "/usr/" 2>/dev/null ; then exec /bin/bash "$@"; else echo "BoB Core services not running"; exit; fi

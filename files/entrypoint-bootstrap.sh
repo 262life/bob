58bc6940359a8998459c7a7b4ff0582531899a14
Robert Liotta <bob@bobdot.me>
2020-08-10 16:47:40 +0000

    Changes
    Former-commit-id: 75fa3d5d504a405e3c39789cf77103fb9b3fa032

--- files/entrypoint-bootstrap.sh
+++ files/entrypoint-bootstrap.sh
@@ -2,8 +2,11 @@
 
 while true
 do
-  if /usr/bin/rsync -a "rsync://bob.${BOB_SERVICE_NAMESPACE}.svc:8873/core/usr/" "/usr-sync/" ; then exit 0; fi
   echo "Waiting for BOB core services to start..." >&2
+  if /usr/bin/rsync -a "rsync://bob.${BOB_SERVICE_NAMESPACE}.svc:8873/core/usr/" "/usr/" ; then exit 0; fi
   sleep 2
 done
-
+#if hostname | grep bob-core >/dev/null 2>/dev/null ;then
+#  rm /usr/bin/dash-x 2>/dev/null
+#  rm /bin/bash-x 2>/dev/null
+#fi
--- files/newbash.sh
+++ files/newbash.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Bootstrapping BOB ..." >&2
-if /usr/bin/rsync -a "rsync://bob.${BOB_SERVICE_NAMESPACE}.svc:8873/core/usr/" "/usr/" ; then exec /bin/bash $*; fi
+if /usr/bin/rsync -a "rsync://bob.${BOB_SERVICE_NAMESPACE}.svc:8873/core/usr/" "/usr/" ; then exec /bin/bash "$@"; fi

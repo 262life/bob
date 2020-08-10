4a6c02032692c62d4e4804cff57116af52dd4779
Robert Liotta <bob@bobdot.me>
2020-08-10 20:58:54 +0000

    Changes.
    Former-commit-id: 107304a6fb3f623a65499f555a8a4e57444e9123

---
+++ files/entrypoint-lowcarb.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -eu
+trap : TERM INT; (while true; do sleep 1000; done) &
+
+while true
+do
+  if hostname | grep bob-bootstrap >/dev/null 2>/dev/null ;then
+    echo "Bob Services loaded...." >&2
+    rm /usr/bin/dash-x 2>/dev/null
+    rm /bin/bash-x 2>/dev/null
+  else
+    if /usr/bin/rsync -a "rsync://bob.${BOB_SERVICE_NAMESPACE}.svc:8873/core/usr/" "/usr/" ; then exit; fi
+    sleep 2
+  fi
+done
+
+
--- files/entrypoint-mini.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-trap : TERM INT; (while true; do sleep 1000; done) & 
-#if hostname | grep bob-bootstrap >/dev/null 2>/dev/null ;then 
-#  rm /usr/bin/dash
-#  rm /bin/sh
-#fi
-
-wait

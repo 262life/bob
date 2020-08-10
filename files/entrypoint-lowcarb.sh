3dc40e7df293d595486da09f94cf0eb596580b91
Robert Liotta <bob@bobdot.me>
2020-08-10 21:33:25 +0000

    Changes
    Former-commit-id: bccca78e17ef28ae3eb8be2f0a5de96456529e42

--- files/entrypoint-lowcarb.sh
+++ files/entrypoint-lowcarb.sh
@@ -4,14 +4,14 @@ trap : TERM INT; (while true; do sleep 1000; done) &
 echo ":${MY_POD_NAME}:"
 while true
 do
-  if  "${MY_POD_NAME}" == "bob-bootstrap"  ;then
-    echo "Bob Services loaded...." >&2
-    rm /usr/bin/dash-x 2>/dev/null
-    rm /bin/bash-x 2>/dev/null
-  else
-    if /usr/bin/rsync -a "rsync://bob.${BOB_SERVICE_NAMESPACE}.svc:8873/core/usr/" "/usr/" ; then exit; fi
-    sleep 2
-  fi
+  case "${MY_POD_NAME}" in
+    "bob-bootstrap"*) echo "Bob Services loaded...." >&2;
+                      rm /usr/bin/dash-x 2>/dev/null;
+                      rm /bin/bash-x 2>/dev/null;;
+    *)                if /usr/bin/rsync -a "rsync://bob.${BOB_SERVICE_NAMESPACE}.svc:8873/core/usr/" "/usr/" ; then exit; fi;
+                      sleep 2;;
+  esac
+
 done
 
 

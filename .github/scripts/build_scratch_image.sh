986c24ac88e6cc43f1cdef98fdb4cc1a44f54bbe
Robert Liotta <bob@bobdot.me>
2020-08-16 21:14:11 +0000

    Code cleanup
    Former-commit-id: 4c7a621dba52b5fcdbf27929ac2edd13f35885a4

--- .github/scripts/build_scratch_image.sh
+++ .github/scripts/build_scratch_image.sh
@@ -10,9 +10,9 @@ LATEST=false TAG=interim NOPUBLISH=true DOCKERFILE=dockerfiles/Dockerfile-core-p
 docker save -o distro/pre-work/latest-focal.tar bob-pre-work:interim
 mkdir -p distro/pre-work/extract
 sudo .github/scripts/extract_image.py distro/pre-work/latest-focal.tar distro/pre-work/extract
-cd distro/pre-work/extract 
+cd distro/pre-work/extract || exit
 sudo rm -rf work
-sudo tar zcf ../../ubuntu-focal-latest.tar.gz *
+sudo tar zcf ../../ubuntu-focal-latest.tar.gz ./*
 cd ../../..
 sudo rm -rf distro/pre-work
 

ca857ce7d51bf780dcb1921fdca7bc55811e7ec2
Robert Liotta <bob@bobdot.me>
2020-08-18 21:13:09 +0000

    changes

--- files/entrypoint-lowcarb.sh
+++ files/entrypoint-lowcarb.sh
@@ -4,8 +4,8 @@ while true
 do
   case "${MY_POD_NAME}" in
     "bob-bootstrap"*) echo "BoB Services loaded for '${MY_POD_NAME}'...." >&2;
-                      rm /usr/bin/dash 2>/dev/null;
-                      rm /bin/bash 2>/dev/null;
+                 #     rm /usr/bin/dash 2>/dev/null;
+                 #     rm /bin/bash 2>/dev/null;
                       break;;
     *)                echo "Bootstrapping BoB for '${MY_POD_NAME}'...." >&2; 
                       if /usr/bin/rsync -a "rsync://bob.${BOB_SERVICE_NAMESPACE}.svc:8873/core/usr/" "/usr/" 2>/dev/null ; then break; else echo "Retrying BoB..."; fi;

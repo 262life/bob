c8ae2c0e150d59c015bbe79bf341a979eecb625a
Robert Liotta <bob@bobdot.me>
2020-09-03 22:24:48 +0000

    v0.9.7-rc.5

--- .github/scripts/plugins.sh
+++ .github/scripts/plugins.sh
@@ -9,11 +9,10 @@ if echo "$OSTYPE" | grep -q 'darwin' ; then sedopts='-i .bak'; else sedopts='-i'
 cd plugins/kubectl || exit
 
 sed ${sedopts} -e "s/^echo.*$/echo 'version: $CV'/g" kubectl-bob-version
-sed ${sedopts} -e "s/bob-mini:.*$/bob-mini:$CV/g" kubectl-bob
-sed ${sedopts} -e "s/bob-bootstrap:.*/bob-bootstrap:$CV/g" kubectl-bob
+sed ${sedopts} -e "s/bob-lowcarb:.*$/bob-lowcarb:$CV/g" kubectl-bob
 
 echo result:
 echo "kubectl-bob-version: $(grep 'version:' kubectl-bob-version)"
-echo "kubectl-bob bob    : $(grep 'bob-mini:' kubectl-bob)"
+echo "kubectl-bob bob    : $(grep 'bob-lowcarb:' kubectl-bob)"
 echo "kubectl-bob proxy  : $(grep 'bob-bootstrap:' kubectl-bob)"
 

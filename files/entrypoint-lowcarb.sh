bc8d0ad191905ff43691ae1742a7eb9d382725b9
Robert Liotta <bob@bobdot.me>
2020-09-03 20:51:43 +0000

    changes
    Former-commit-id: 49473853314a9823e064972a004c81fe37511fdb

--- files/entrypoint-lowcarb.sh
+++ files/entrypoint-lowcarb.sh
@@ -8,7 +8,12 @@ do
                       rm /bin/bash 2>/dev/null;
                       break;;
     *)                echo "Bootstrapping BoB for '${MY_POD_NAME}'...." >&2; 
-                      if /usr/bin/rsync -a "rsync://bob.${BOB_SERVICE_NAMESPACE}.svc:8873/core/usr/" "/usr/" 2>/dev/null ; then break; else echo "Retrying BoB..."; fi;
+                      if /usr/bin/rsync -a "rsync://bob.${BOB_SERVICE_NAMESPACE}.svc:8873/core/usr/" "/usr/" --exclude bin/bash 2>/dev/null ; then
+                        if /usr/bin/rsync -a "rsync://bob.${BOB_SERVICE_NAMESPACE}.svc:8873/core/usr/bin/bash" "/usr/" 2>/dev/null ; then break; else echo "Retrying BoB..."; fi;
+                      else 
+                        echo "Retrying BoB..."; 
+                      fi;
+
                       sleep 2;;
   esac
 done
